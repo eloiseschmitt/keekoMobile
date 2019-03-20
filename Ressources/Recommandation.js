6458a9a23082935f25701cfa18e6894eb6b45f96
Orbiteo <eschmitt18@gmail.com>
2019-03-20 20:12:00 +0100

    modifs pour test recommandation.js

--- Ressources/Recommandation.js
+++ Ressources/Recommandation.js
@@ -1,7 +1,9 @@
+fimport React, { Component } from 'react'
 import Recommandations from './TotalRecommandations'
+import { Text, FlatList,  } from 'react-native'
 // Fonction permettant de retourner les recommandations en f° des réponses données:
 
-export function getRecommandation (reponse1, reponse2, reponse3, reponse4, reponse5, reponse6, reponse7, reponse8, reponse9, reponse10) {
+/*export function getRecommandation (reponse1, reponse2, reponse3, reponse4, reponse5, reponse6, reponse7, reponse8, reponse9, reponse10) {
   var totalRecommandations = Recommandations
   var listeDesRecommandations = [];
 
@@ -14,12 +16,16 @@ export function getRecommandation (reponse1, reponse2, reponse3, reponse4, repon
   if(reponse3 == "1") {
     listeDesRecommandations.push(totalRecommandations[2])
   }
-  if(reponse4 == "1") {
+  if(reponse4 == "1) {
     listeDesRecommandations.push(totalRecommandations[3])
   }
-  if(reponse5 == "1") {
+  if(reponse5 == 1) {
     listeDesRecommandations.push(totalRecommandations[4])
   }
 
   return listeDesRecommandations;
-}
+}*/
+
+export default const Recommandation = ({Recommandations}) => (
+
+)
