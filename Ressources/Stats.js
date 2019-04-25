408dcb2abdf582dc4a7bdadad7713f9dc5b27463
Eloïse Schmitt <eloiseschmitt@iMac-de-Eloise.local>
2019-04-25 06:47:26 +0200

    maj stats.js

--- Ressources/Stats.js
+++ Ressources/Stats.js
@@ -2,17 +2,55 @@
 export function haveStats (totalIdQuestions, totalIdAnswers, listAnswers) {
 
 let nombreDeQuestions = listAnswers.nbDeReponses
-console.log(nombreDeQuestions)
 
-//Sortir les stats générales:
-//Faire un appel en bdd pour obtenir le nombre de questions:
+for(let i=1 ; i<=nombreDeQuestions : i++) {
+  for(let j=1 ; j<=14 ; j++) {
+    if(listAnswers.answers.question_id == j) {
+      let ${question}+j = listAnswers.answers.id
+      let ${nbDeOuiQuestion}+j
+      let ${nbDeNonQuestion}+j
+      let ${plusDeOuiQueDeNon}+j
+      if(listAnswers.answers.reponse == 1) {
+        ${nbDeOuiQuestion}+j = (${nbDeOuiQuestion}+j) + 1
+      }
+      else{
+        ${nbDeNonQuestion}+j = (${nbDeNonQuestion}+j) + 1
+      }
+      if(${nbDeOuiQuestion}+j > ${nbDeNonQuestion}+j) {
+        ${plusDeOuiQueDeNon}+j = 1
+      }
+    } else if(listAnswers.answers.question_id == 10) {
+      let question10 = listAnswers.answers.id
+      let nbDeOuiQuestion10
+      let nbDeNonQuestion10
+      let plusDeOuiQueDeNon10
+      if(listAnswers.answers.reponse == 1) {
+        nbDeOuiQuestion10++
+      }
+      else{
+        nbDeNonQuestion10++
+      }
+      if(nbDeOuiQuestion10 > nbDeNonQuestion10) {
+        plusDeOuiQueDeNon10 = 1
+      }
+    } else if(listAnswers.answers.question_id == 11) {
+      let question11 = listAnswers.answers.id
+      let nbDeOuiQuestion11
+      let nbDeNonQuestion11
+      let plusDeOuiQueDeNon11
+      if(listAnswers.answers.reponse == 1) {
+        nbDeOuiQuestion11++
+      }
+      else{
+        nbDeNonQuestion11++
+      }
+      if(nbDeOuiQuestion11 > nbDeNonQuestion11) {
+        plusDeOuiQueDeNon11 = 1
+      }
+    }
+  }
+}
 
-
-
-
-/*for(let i=1 ; i<=nombreDeQuestions : i++) {
-
-}*/
 
   /*let stat = {
     secteur: "marché",
